483482eedca808e3074b35214399580528fb76a7
Pranav A <worldly.music.cafe@gmail.com>
2017-01-04 22:44:16 +0530

    Added skip grams

--- sgrams.cpp
+++ sgrams.cpp
@@ -11,17 +11,15 @@ main(){
 	std::cout << "Enter the string\n";
 	std::cin >> input;
 	std::vector<std::string> output = sgrams(input, 1);
+	for(auto& i : output)
+		std::cout << i << std::endl;
 }
 
 std::vector<std::string> sgrams(std::string input, int skip)
 {
 	std::vector<std::string> result;
 	//This function splits the word into s-grams. Like abcd with s=1 to (ac, bd)
-
 	for(int i = 0; i != input.length() - skip - 1; i++)
-	{
-		// result.push_back(input.substr(i, i + 1) + input.substr(i + skip, i + skip + 1));
-		std::cout << input.substr(i, skip) + input.substr(i + skip + 1, skip) << std::endl;
-	}
+		result.push_back(input.substr(i, skip) + input.substr(i + skip + 1, skip));
 	return result;
 }
