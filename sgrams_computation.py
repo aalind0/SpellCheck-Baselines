36beced3a6e60e5ee8755686d464a8e06521ef32
Aalind Singh <singh.aalind@gmail.com>
2017-01-06 00:00:15 +0530

    Update

--- sgrams_computation.py
+++ sgrams_computation.py
@@ -1,6 +1,7 @@
 query = "toiilet"
 skips = [0, 1]
 
+in_file1 = open("wordlist.txt", "r+")
 
 def sgrams(entry, skips):
         result = set()
@@ -15,7 +16,16 @@ b_query = sgrams(query, skips)
 
 in_file = open("wordlist_sgrams.txt", "r+")
 
+sgram_dict = dict()
+
+for i in in_file1:
+    sgram_dict[i.rstrip()] = sgrams(i.rstrip(), skips)
+    #sgram_dict = {i : sgrams(i.rstrip(), skips)}
+    
+print sgram_dict['toilet']
+
 maxkey = 0
+
 for line in in_file:
     values = set(line.split())
     jaccard =  float(len(values.intersection(b_query))) / float(len(values.union(b_query)))
