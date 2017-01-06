8ced536d8124ae7e42cc2b08c767b36cc5775979
Pranav A <worldly.music.cafe@gmail.com>
2017-01-06 19:24:27 +0530

    Almost sgrams complete

--- sgrams_computation.py
+++ sgrams_computation.py
@@ -1,34 +1,39 @@
-query = "toiilet"
+import heapq
+query = raw_input("Enter here unfortunate misspelled word: ")
 skips = [0, 1]
 
 in_file1 = open("wordlist.txt", "r+")
+in_file = open("wordlist_sgrams.txt", "r+")
 
+# Breaking words into s-grams
 def sgrams(entry, skips):
-        result = set()
-        for skip in skips:
-                for i in range(0, len(entry) - skip - 1):
-                        result.add(entry[i] + entry[i + skip + 1])
-        if len(entry) < 2:
-                result.add(entry)
-        return result
+		result = set()
+		for skip in skips:
+				for i in range(0, len(entry) - skip - 1):
+						result.add(entry[i] + entry[i + skip + 1])
+		if len(entry) < 2:
+				result.add(entry)
+		return result
 
 b_query = sgrams(query, skips)
 
-in_file = open("wordlist_sgrams.txt", "r+")
-
+#Sgram_dict is a dictionary which has format word:{s-gram}
 sgram_dict = dict()
-
 for i in in_file1:
-    sgram_dict[i.rstrip()] = sgrams(i.rstrip(), skips)
-    #sgram_dict = {i : sgrams(i.rstrip(), skips)}
-    
-print sgram_dict['toilet']
+	sgram_dict[i.rstrip()] = sgrams(i.rstrip(), skips)
 
-maxkey = 0
+sgram_sort = dict() #Sgram_dict is a dictionary which has format word:jaccard
+k = 10
+#Loop in dictionary to calculate jaccards
+for word in sgram_dict:
+	values = sgram_dict[word]
+	jaccard =  float(len(values.intersection(b_query))) / float(len(values.union(b_query)))
+	if len(sgram_sort) < k:
+		sgram_sort[word] = jaccard
+	if len(sgram_sort) == k:
+		if sgram_sort[min(sgram_sort, key=sgram_sort.get)] < jaccard:
+			del sgram_sort[min(sgram_sort, key=sgram_sort.get)]
+			sgram_sort[word] = jaccard
 
-for line in in_file:
-    values = set(line.split())
-    jaccard =  float(len(values.intersection(b_query))) / float(len(values.union(b_query)))
-    if jaccard > maxkey:
-        maxkey = jaccard
-        print line
+for w in sorted(sgram_sort, key=sgram_sort.get, reverse=True):
+  print w, sgram_sort[w]
